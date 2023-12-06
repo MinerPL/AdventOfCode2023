066370992ba77953cb4045df2cc5121bbf843c52
Dawid Jaworski <contact@minerpl.xyz>
2023-12-06 16:16:24 +0100

    Task 6.1

---
+++ Task6.1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Task6._1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = "";
+            if (File.Exists("./input.txt"))
+            {
+                input = File.ReadAllText("./input.txt");
+            }
+            else
+            {
+                Console.WriteLine("Missing input.txt file");
+                Console.ReadKey();
+            }
+            
+            string[] lines = input.Split('\n');
+            string[] time = lines[0].Split(':')[1].Trim().Split(' ').Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            string[] distance = lines[1].Split(':')[1].Trim().Split(' ').Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            
+            var sum = 1;
+            for (int i = 0; i < time.Length; i++)
+            {
+                var sum2 = 0;
+
+                var raceTime = int.Parse(time[i]);
+                var raceDistance = int.Parse(distance[i]);
+                for (int j = 0; j < raceTime; j++)
+                {
+                    var calc = j * (raceTime - j);
+                    if (calc > raceDistance)
+                    {
+                        sum2++;
+                    }
+                }
+                
+                sum *= sum2;
+            }
+            
+            Console.WriteLine(sum);
+        }
+    }
+}

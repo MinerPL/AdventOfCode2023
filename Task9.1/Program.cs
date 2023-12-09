931c500f58cd6e27981606aecb049894bd3eb5c4
Dawid Jaworski <contact@minerpl.xyz>
2023-12-09 12:23:40 +0100

    Task 9.1

---
+++ Task9.1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Task9._1
+{
+    internal class Program
+    {
+
+        public static int[] diffCalc(int[] numbers)
+        {
+            var diff = new int[numbers.Length - 1];
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                if(i == numbers.Length - 1)
+                    break;
+                diff[i] = numbers[i + 1] - numbers[i];
+            }
+
+            return diff;    
+        }
+        
+        public static void Main(string[] args)
+        {
+            var lines = (File.ReadAllText("input.txt")).Replace("\r", "")
+                .Split(new string[] { "\n" }, StringSplitOptions.None);
+
+            var sum = 0;
+            foreach (var line in lines)
+            {
+                var numbers = line.Split(' ').Select(int.Parse).ToArray();
+                var diff = numbers;
+                var last = new List<int>();
+                while (!diff.All(x => x == 0))
+                {
+                    diff = diffCalc(diff);
+                    last.Add(diff.Last());
+                }
+
+                sum += numbers.Last() + last.Aggregate((a, b) => a + b);
+            }
+            
+            Console.WriteLine(sum);
+        }
+    }
+}
